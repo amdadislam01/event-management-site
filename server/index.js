dc71377ebcc0b3ea303da8592f9f22f7b29fc056
amdadislam01 <amdadislam733@gmail.com>
2025-11-24 15:10:43 +0600

    server added and events api make complate

---
+++ server/index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cewig2g.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("eventFlowDB");
+    const eventCollection = db.collection("events");
+
+    // Event Create API
+    app.post("/events", async (req, res) => {
+      const event = req.body;
+      // event Create Time
+      event.createAt = new Date();
+      const result = await eventCollection.insertOne(event);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("event management application !");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
