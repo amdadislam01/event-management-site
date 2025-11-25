b55c4249ba4c2b3edbcb7310cc76e3046d1def19
amdadislam01 <amdadislam733@gmail.com>
2025-11-25 19:19:01 +0600

    Testimonials section complate

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import RecentEvents from "@/components/RecentEvents";
+import Testimonials from "@/components/Testimonials";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <RecentEvents />
       <Features />
+      <Testimonials />
     </>
   );
 }
