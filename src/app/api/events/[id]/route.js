d3da1b834b8ab8348a6c7bb458aade26b7c021f6
amdadisalm01 <amdadislam733@gmail.com>
2026-04-09 22:34:41 +0600

    MODIFIED: added PUT method to update event details by ID

--- src/app/api/events/[id]/route.js
+++ src/app/api/events/[id]/route.js
@@ -32,3 +32,32 @@ export async function DELETE(req, { params }) {
     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
+
+export async function PUT(req, { params }) {
+  try {
+    const { id } = await params;
+    const client = await clientPromise;
+    const db = client.db("eventFlowDB");
+    const eventCollection = db.collection("events");
+
+    const updatedData = await req.json();
+    
+    // Remove _id from updatedData to avoid error when updating
+    delete updatedData._id;
+    
+    // Ensure date is a Date object if provided
+    if (updatedData.date) {
+      updatedData.date = new Date(updatedData.date);
+    }
+
+    const result = await eventCollection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: updatedData }
+    );
+
+    return NextResponse.json(result);
+  } catch (error) {
+    console.error("Database Error:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
